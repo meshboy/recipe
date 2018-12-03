8afdc040aeccb3666dde5457bcd361cc517b13bc
Meshileya Seun <meshileyaseun@gmail.com>
2018-12-03 04:49:46 +0100

    remove redundancy

--- server/src/server.js
+++ server/src/server.js
@@ -11,25 +11,3 @@ const path = '/recipe'
 graphQLRouter.applyMiddleware({ app, path});
 
 export default app;
-
-
-// import Koa from 'koa';
-// import { graphqlUploadKoa } from 'graphql-upload';
-
-// import setMiddleware from './middleware';
-// import { graphQLRouter } from './api';
-// import { connect } from './db';
-
-// const app = new Koa().use(
-//     graphqlUploadKoa({
-//         maxFileSize: process.env.FILE_SIZE,
-//         maxFiles: 1
-//     })
-// );
-// connect();
-// setMiddleware(app);
-
-// const path = '/recipe'
-// graphQLRouter.applyMiddleware({ app, path});
-
-// export default app;
